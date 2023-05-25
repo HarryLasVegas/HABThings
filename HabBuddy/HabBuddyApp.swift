7540ef11f3a5ecdf7debdfd7335910caf6b1bb28
HarryLasVegas <info@harrylasvegas.de>
2023-05-25 13:01:14 +0200

    Initial Commit

---
+++ HabBuddy/HabBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabBuddyApp.swift
+//  HabBuddy
+//
+//  Created by Stephan Weber on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct HabBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
